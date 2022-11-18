3093696465ac61fed5ae6cd405a229e0512e6752
rachelcnc <115052360+rachelcnc@users.noreply.github.com>
2022-11-18 08:50:10 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,13 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
     led.setBrightness(input.lightLevel())
 })
+basic.forever(function () {
+    led.setBrightness(255 - input.lightLevel())
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
